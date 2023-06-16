ec4a8edd031617dd941a1ddb621323fa8e96da23
threepolesoft <threepolesoft@gmail.com>
2023-06-16 22:56:11 +0600

    init

---
+++ WindowsFormAppSerialKeyGenerate/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormAppSerialKeyGenerate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtSl = new System.Windows.Forms.TextBox();
+            this.btnValid = new System.Windows.Forms.Button();
+            this.lbCode = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(103, 109);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(110, 36);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtSl
+            // 
+            this.txtSl.Location = new System.Drawing.Point(219, 118);
+            this.txtSl.Name = "txtSl";
+            this.txtSl.Size = new System.Drawing.Size(242, 20);
+            this.txtSl.TabIndex = 1;
+            // 
+            // btnValid
+            // 
+            this.btnValid.Location = new System.Drawing.Point(512, 109);
+            this.btnValid.Name = "btnValid";
+            this.btnValid.Size = new System.Drawing.Size(110, 36);
+            this.btnValid.TabIndex = 0;
+            this.btnValid.Text = "Valid";
+            this.btnValid.UseVisualStyleBackColor = true;
+            this.btnValid.Click += new System.EventHandler(this.btnValid_Click);
+            // 
+            // lbCode
+            // 
+            this.lbCode.AutoSize = true;
+            this.lbCode.Location = new System.Drawing.Point(476, 121);
+            this.lbCode.Name = "lbCode";
+            this.lbCode.Size = new System.Drawing.Size(0, 13);
+            this.lbCode.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 359);
+            this.Controls.Add(this.lbCode);
+            this.Controls.Add(this.txtSl);
+            this.Controls.Add(this.btnValid);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtSl;
+        private System.Windows.Forms.Button btnValid;
+        private System.Windows.Forms.Label lbCode;
+    }
+}
+
---
+++ WindowsFormAppSerialKeyGenerate/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+
+namespace WindowsFormAppSerialKeyGenerate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+    
+        string Hash = "";
+        int iAccess = 0;
+        string PrivateSalt = "Alpha";
+        string SourceString = "";
+        string guidString;
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+
+
+            Guid theGuid;
+
+
+            while (true)
+            {
+                theGuid =  Guid.NewGuid();
+                guidString = theGuid.ToString().Replace("-", "").Substring(0, 16);
+                SourceString = guidString + "|" + iAccess.ToString() + "|" + PrivateSalt;
+
+                byte[] data = Encoding.Default.GetBytes(SourceString);
+
+                CryptographicException Crypto = new CryptographicException();
+
+                Hash = GenerateSHA512String(SourceString);
+              
+          
+                if (Hash.StartsWith(GetDiff()))
+                {
+                    break;
+                }
+
+                iAccess++;
+            }
+
+            string str = GetDiff();
+
+            string s1, s2, s3, s4;
+            s1 = guidString.Substring(0, 4);
+            s2 = guidString.Substring(4, 4);
+            s3 = guidString.Substring(8, 4);
+            s4 = guidString.Substring(12, 4);
+            string serial = s1 + "-" + s2 + "-" + s3 + "-" + s4;
+
+            this.txtSl.Text = serial;
+            this.lbCode.Text = str;
+
+        }
+
+        private string GetDiff()
+        {
+            return iAccess.ToString();
+        }
+
+
+        public static string GenerateSHA512String(string inputString)
+        {
+            SHA512 sha512 = SHA512Managed.Create();
+            byte[] bytes = Encoding.UTF8.GetBytes(inputString);
+            byte[] hash = sha512.ComputeHash(bytes);
+            return GetStringFromHash(hash);
+        }
+
+        private static string GetStringFromHash(byte[] hash)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                result.Append(hash[i].ToString("X2"));
+            }
+            return result.ToString();
+        }
+
+        public bool IsSerialValid(string serialCode, string ProductCode)
+        {
+            string SourceString = serialCode.Replace("-", "") + "|" + ProductCode + "|" + "Alpha";
+
+            byte[] data = Encoding.Default.GetBytes(SourceString);
+
+            Hash = GenerateSHA512String(SourceString);
+
+            if (Hash.StartsWith(GetDiff()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void btnValid_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(IsSerialValid(this.txtSl.Text, this.lbCode.Text).ToString());
+        }
+    }
+}
